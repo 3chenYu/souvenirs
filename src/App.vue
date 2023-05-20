e7f8127897ac1383da3f563f910cce689b8d7e43
胡歌 <318032354@qq.com>
2023-05-20 15:09:11 +0800

    琪琪

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,15 @@
 </script>
 
 <template>
-  <div>
-    宋晓琪
+  <div class="qi_love">
+    
   </div>
 </template>
 
 <style scoped>
-
+.qi_love{
+  width: 500px;
+  height: 500px;
+  background: goldenrod;
+}
 </style>
